7bcb8db9d8beb1865c263b99db8ed92ee6c25c13
DFS <emailanda@gmail.com>
2026-04-13 13:24:37 +0800

    fix migration payment_methods name column

--- database/migrations/2026_04_13_044104_add_name_to_payment_methods_table.php
+++ database/migrations/2026_04_13_044104_add_name_to_payment_methods_table.php
@@ -1,3 +1,26 @@
-Schema::table('payment_methods', function (Blueprint $table) {
-    $table->string('name')->after('id');
-});
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('payment_methods', function (Blueprint $table) {
+            if (!Schema::hasColumn('payment_methods', 'name')) {
+                $table->string('name')->nullable()->after('id');
+            }
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('payment_methods', function (Blueprint $table) {
+            if (Schema::hasColumn('payment_methods', 'name')) {
+                $table->dropColumn('name');
+            }
+        });
+    }
+};
